d4680e15ec3d0674208d4c8a5031b3a6e5adb2eb
Karan Vyas <karanvyas2907@gmail.com>
2022-02-01 16:44:13 +0530

    added styles through css

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const searchInput = document.querySelector("#search-word");
 const searchbtn = document.querySelector("#search-btn");
 const meaningContainer = document.querySelector(".meaning-container");
+const loadingDiv=document.querySelector('#load')
 let audio;
+
 searchbtn.addEventListener("click", (e) => {
-  meaningContainer.innerHTML = "";
+ 
+  loadingDiv.classList.add('load');
+ 
   if (searchInput.value != "") {
+    loadingDiv.classList.add('load');
+     meaningContainer.innerHTML = loadingDiv.outerHTML;
     console.log(searchInput.value);
     fetch(
       `https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput.value}`
@@ -13,14 +19,28 @@ searchbtn.addEventListener("click", (e) => {
         return response.json();
       })
       .then((data) => {
+        loadingDiv.classList.remove('load');
+        meaningContainer.innerHTML = loadingDiv.outerHTML;
         if (data.title) {
-          console.log("not found");
-        } else {
+          console.log(data.title);
+          const wordContainer = document.createElement("div");
+
+          wordContainer.classList.add("word-container");
+          meaningContainer.innerHTML=`
+          <h2>${data.title}</h2>
+          <p class="definition"> ${data.message}</p>
+          <p class="definition">${data.resolution}</p>
+        `;
+        } else 
           meaning(data);
-        }
+
       })
       .catch(() => {
-        console.log("not found");
+        console.log(data);
+        // meaningContainer.innerHTML=`
+        //    <h2 class="word"></h2>
+        //    <p class="definition"> ${data.message}</p>
+        //  `;
       });
   }
 
@@ -37,7 +57,9 @@ function meaning(data) {
   wordContainer.innerHTML = `
                 <h2 class="word">${data[0].word}</h2>
                 <div class="audio-phonetic">
-                <button class="volume">Audio</button>
+                <button class="volume">
+                <i class="fa fa-volume-up fa-lg" aria-hidden="true"></i>
+                </button>
                 <p class="phonetic">/${data[0].phonetic}/</p>
                 </div>
                 
@@ -52,13 +74,14 @@ function meaning(data) {
             `;
     meaningElement.definitions.forEach((definitionsElement,index) => {
       wordContainer.innerHTML += `
+                <div class="content">
                 <p class="definition">${index+1}. ${definitionsElement.definition}</p>
-                <p class="example">example: ${definitionsElement.example} </p>
+                <p class="example">"${definitionsElement.example}" </p>
                 <p class="similar">similar: ${definitionsElement.synonyms.slice(
                   0,
                   5
                 )}</p>
-                <br>
+                </div>
                 </div>
                 `;
     });
